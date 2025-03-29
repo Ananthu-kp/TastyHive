89bfc9d302859044a0a209f11af212f8d391fd92
Ananthakrishnan KP <ananthukp17@gmail.com>
2025-03-29 20:22:10 +0530

    final

--- Frontend/src/config/axios.js
+++ Frontend/src/config/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://tasty-hive-9nqd.vercel.app/',
     withCredentials: true,
 });
 
